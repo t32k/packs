c8483945b712c147048ca55a70e696783d089c70
t32k <ijok.ijok@gmail.com>
2014-02-18 17:05:22 +0900

    Add img dir

--- js/app.js
+++ js/app.js
@@ -6,10 +6,30 @@ exec('gem list', function(err, stdout, stderr) {
   var contents = "";
   var name = stdout.match( /\w+\s/g );
   var version = stdout.match( /\(.+\)/g );
+      version = version.map(function(value){
+        return value.replace(/\(|\)|\,.+/g, '');
+      });
+  
+  
   name.forEach(function(element, index, array){
-    contents += '<li><h3 class="name">' + element + '</h3><p class="version">version <b>'+ version[index] +'</b></p></li>';
+    switch (element) {
+      case 'sass ':
+        contents += '<li><h3 class="name sass">' + element + '</h3><p class="version">version <b>'+ version[index] +'</b></p></li>';
+        break;
+      case 'compass ':
+        contents += '<li><h3 class="name compass">' + element + '</h3><p class="version">version <b>'+ version[index] +'</b></p></li>';
+        break;
+      case 'jekyll ':
+        contents += '<li><h3 class="name jekyll">' + element + '</h3><p class="version">version <b>'+ version[index] +'</b></p></li>';
+        break;
+      case 'bundler ':
+        contents += '<li><h3 class="name bundler">' + element + '</h3><p class="version">version <b>'+ version[index] +'</b></p></li>';
+        break;
+      default:
+        contents += '<li><h3 class="name">' + element + '</h3><p class="version">version <b>'+ version[index] +'</b></p></li>';
+    }
+    ;
   });
   list.innerHTML = contents;
-  console.log('name is= '+ name);
-  console.log('version is= '+ version);
+  
 });
